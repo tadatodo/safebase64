8af5fe1a7b9a426bb8079ab37bf52e81f8208414
codeallthethingz <howdoicontactwill@gmail.com>
2024-06-18 10:41:28 -0700

    initial

---
+++ safe-base64.go
@@ -0,0 +1,90 @@
+package safebase64
+
+import (
+	"fmt"
+	"math/rand"
+	"regexp"
+	"time"
+)
+
+const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+
+type Base64 struct {
+	BlockList []*regexp.Regexp
+	Rand      *rand.Rand
+}
+
+// New creates a new Base64 instance with the given blockList.
+// The blockList is a case-insensitive list of words that should not be generated and
+// will be converted to regexes with common vowel<>number swaps, e.g. a<>4 e<>3.
+func New(blockList []string) *Base64 {
+	return NewWithRand(blockList, rand.New(rand.NewSource(time.Now().UnixNano())))
+}
+
+// New creates a new Base64 instance with the given blockList.
+// The blockList is a case-insensitive list of words that should not be generated and
+// will be converted to regexes with common vowel<>number swaps, e.g. a<>4 e<>3.
+// The random number generator r is used to generate the random strings.
+func NewWithRand(blockList []string, r *rand.Rand) *Base64 {
+	if r == nil {
+		panic("rand is nil")
+	}
+	return &Base64{
+		BlockList: generateRegex(blockList),
+		Rand:      r,
+	}
+}
+
+// take the list of words and generate regexes where numbers could be inserted, ensure case insensitivity, 1=i, 3=e, 4=a, 5=s, 0=o anywhere in the string
+func generateRegex(blockList []string) []*regexp.Regexp {
+	var regexList []*regexp.Regexp
+	i1Regex := regexp.MustCompile("1|i")
+	e3Regex := regexp.MustCompile("3|e")
+	a4Regex := regexp.MustCompile("4|a")
+	s5Regex := regexp.MustCompile("5|s")
+	o0Regex := regexp.MustCompile("0|o")
+
+	for _, word := range blockList {
+		// replace 1 with i, 3 with e, 4 with a, 5 with s, 0 with o
+		word = i1Regex.ReplaceAllString(word, "[1i]")
+		word = e3Regex.ReplaceAllString(word, "[3e]")
+		word = a4Regex.ReplaceAllString(word, "[4a]")
+		word = s5Regex.ReplaceAllString(word, "[5s]")
+		word = o0Regex.ReplaceAllString(word, "[0o]")
+		// case insensitive
+		word = "(?i)" + word
+		fmt.Println("generated regex: ", word)
+		regexList = append(regexList, regexp.MustCompile(word))
+	}
+	return regexList
+}
+
+// Generate generates a random string of length n using the charset.
+// It avoids generating strings containing words in the block list and will try 100 new strings before panicing.
+func (ba *Base64) Generate(n int) string {
+	if n <= 0 {
+		return ""
+	}
+	for i := 0; i < 100; i++ {
+		b := make([]byte, n)
+		for i := range b {
+			num := ba.Rand.Intn(len(charset))
+			b[i] = charset[num]
+		}
+		str := string(b)
+		if !ba.ContainsSwearWord(str) {
+			return str
+		}
+	}
+	panic("could not generate a safe string")
+}
+
+// ContainsSwearWord checks if the generated string contains any swear words.
+func (ba *Base64) ContainsSwearWord(str string) bool {
+	for _, regex := range ba.BlockList {
+		if regex.MatchString(str) {
+			return true
+		}
+	}
+	return false
+}
---
+++ safe-base64_test.go
@@ -0,0 +1,61 @@
+package safebase64
+
+import (
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenerateBadWords(t *testing.T) {
+	producesBadWord := NewWithRand([]string{}, rand.New(rand.NewSource(1)))
+	badWord := "FTZidia3NjuA"
+
+	found := false
+	for i := 0; i < 10; i++ {
+		got := producesBadWord.Generate(12)
+		if got == badWord {
+			found = true
+			break
+		}
+	}
+	require.True(t, found)
+
+	doesntProduceBadWord := NewWithRand([]string{"Diaen"}, rand.New(rand.NewSource(1)))
+
+	for i := 0; i < 10; i++ {
+		got := doesntProduceBadWord.Generate(12)
+		if got == badWord {
+			t.Errorf("Generate() = %v, never want %v", got, badWord)
+		}
+	}
+}
+
+func TestGenerate(t *testing.T) {
+	b := New([]string{})
+	require.Equal(t, 0, len(b.Generate(0)))
+	require.Equal(t, 0, len(b.Generate(-1)))
+	require.Equal(t, 12, len(b.Generate(12)))
+	require.Equal(t, 130, len(b.Generate(130)))
+}
+
+func TestGeneratEdges(t *testing.T) {
+	b := NewWithRand(nil, rand.New(rand.NewSource(1)))
+	require.Equal(t, 10, len(b.Generate(10)))
+}
+
+func TestPanics(t *testing.T) {
+	require.Panics(t, func() {
+		NewWithRand(nil, nil)
+	})
+
+	// split alphabet into array
+	alphabet := make([]string, len(charset))
+	for i, c := range charset {
+		alphabet[i] = string(c)
+	}
+
+	require.Panics(t, func() {
+		New(alphabet).Generate(10)
+	})
+}
